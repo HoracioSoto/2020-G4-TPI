2032aee22a54e0e688f1a490343f150bd6100a2e
Miguel <miguelbritos91@gmail.com>
2020-06-18 01:18:35 -0300

    se agrega chartsjs

--- frontend/src/components/reporte/Reporte.js
+++ frontend/src/components/reporte/Reporte.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import {Doughnut,Line} from 'react-chartjs-2';
 import './Reporte.css'
+
 
 
 export class Reporte extends Component {
@@ -11,7 +13,54 @@ export class Reporte extends Component {
         hospitales: [],
         provincias: [],
         fecha: '',
+        datos: {
+            labels: [
+                'Asintomáticos',
+                'Sintomáticos',
+                'Fallecidos'
+            ],
+            datasets: [{
+                data: [300, 50, 25],
+                backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+                ],
+                hoverBackgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+                ]
+            }]
+        },
+        datosline: {
+            labels: ['Mar', 'Abr', 'May', 'Jun', 'Jul','Ago','Sep','Oct','Nov'],
+            datasets: [
+              {
+                label: 'Contagios por mes',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [20, 59, 280, 481, 1200]
+              }
+            ]
+          },
     }
+    
 
     async componentDidMount() {
         await this.getHospitales()
@@ -106,9 +155,20 @@ export class Reporte extends Component {
                             <li>
                                 <p>Contagios por Provincias:</p>
                             </li>
+                                <ul>
+                                    <li></li>
+                                </ul>
                         </ul>
-
-
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h6>Estadistica de Pacientes Por Estado Actual:</h6>
+                        <Doughnut data={this.state.datos} />
+                    </div>
+                    <div className="col-md-6">
+                        <h6>Curva de contagios mensual:</h6>
+                        <Line data={this.state.datosline} />
                     </div>
                 </div>
             </div>
