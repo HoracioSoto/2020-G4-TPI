22990eb7fc1428736d04837bcf5ee873e386fcae
Miguel <miguelbritos91@gmail.com>
2020-06-23 02:41:23 -0300

    Reporte Curva de contagios mensual

--- frontend/src/components/reporte/ReporteMensual.js
+++ frontend/src/components/reporte/ReporteMensual.js
@@ -5,7 +5,7 @@ import {Line} from 'react-chartjs-2';
 export class ReporteMensual extends Component {
     state = {
         datosMeses: {
-            labels: ['Mar', 'Abr', 'May', 'Jun', 'Jul','Ago','Sep','Oct','Nov'],
+            labels: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
             datasets: [
               {
                 label: 'Contagios por mes',
@@ -26,11 +26,75 @@ export class ReporteMensual extends Component {
                 pointHoverBorderWidth: 2,
                 pointRadius: 1,
                 pointHitRadius: 10,
-                data: [20, 59, 280, 481, 1200, 2900]
+                data: []
               }
             ]
           },
     }
+
+    async componentDidMount() {
+      await this.getPacientes()
+      await this.getPacientesMes()
+  }
+
+  getPacientes = async () => {
+      const res = await axios.get('http://localhost:8000/api/paciente/')
+      this.setState({ pacientes: res.data })
+      //console.log(this.state.pacientes)
+  }
+
+  getPacientesMes = async () => {
+    let pacientes = await this.state.pacientes
+    if(pacientes) pacientes = pacientes.sort((a,b) => a.fecha_creacion.localeCompare(b.fecha_creacion))
+    
+    let mesActual = new Date().getMonth()
+    // console.log(mesActual)
+
+    let meses = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic']
+    
+    let mesesTranscurridos = meses.slice(0,mesActual+1)
+
+    let pacientesMes = []
+    for (let i = 0; i < mesActual+1; i++) {
+      pacientesMes.push(0)
+    }
+
+    for (let i = 0; i < pacientesMes.length; i++) {
+      const filtro = pacientes.filter(paciente => (new Date(Date.parse(paciente.fecha_creacion))).getMonth() === i )
+      if(filtro.length > 0) pacientesMes[i]=filtro.length
+      // console.log(filtro.length)
+    }
+    console.log(pacientesMes)
+    this.setState({
+      datosMeses: {
+        labels: mesesTranscurridos,
+        datasets: [
+          {
+            label: 'Contagios por mes 2020',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: pacientesMes
+          }
+        ]
+      }
+    })
+  }
+
 
     render() {
         return (
